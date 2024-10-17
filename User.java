6efc743ae6c46bea2b4d2de76088c1e818058548
Ashleigh-T <ash0606@me.com>
2024-10-17 15:54:46 -0700

    added basic functionality to the User class

--- User.java
+++ User.java
@@ -1,9 +1,12 @@
 public class User {
+
+    // basic information for a user 
     private int id;
     private String firstName;
     private String lastName;
     private int acess;
 
+    // basic constructor 
     public User(int id, String firstName, String lastName, int acess){
         this.id = id;
         this.firstName = firstName;
@@ -12,10 +15,37 @@ public class User {
 
     }
 
+    //basic copy constructor 
     public User(User other){
         this.id = other.id;
         this.firstName = other.firstName;
         this.lastName = other.lastName;
         this.acess = other.acess;
+    }
+
+    // METHODS  
+    // returns the id of the current user 
+    public int getID(){
+        return this.id;
+    }
+
+    // returns the firstName of the current user 
+    public String getFN(){
+        return this.firstName;
+    }
+
+    // returns the lastName of the current user 
+    public String getLN(){
+        return this.lastName;
+    }
+
+    // returns the acess level of the current user 
+    public int getAcess(){
+        return this.acess;
+    }
+
+    // Changes the acess of the current user 
+    public void changeAcess(int newAcess){
+        this.acess = newAcess;
     }
 }
