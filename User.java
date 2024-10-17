65e5fc3c58fc59a44c21be0d6a3e3cb0bf66a2ef
Ashleigh-T <ash0606@me.com>
2024-10-17 15:43:25 -0700

    added instance vars, constructor and copy const.

--- User.java
+++ User.java
@@ -1,3 +1,21 @@
 public class User {
-    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int acess;
+
+    public User(int id, String firstName, String lastName, int acess){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.acess = acess;
+
+    }
+
+    public User(User other){
+        this.id = other.id;
+        this.firstName = other.firstName;
+        this.lastName = other.lastName;
+        this.acess = other.acess;
+    }
 }
