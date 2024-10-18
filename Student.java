8a6fa74967bbbfad6ff9a7a5608ed47cf064e3de
Ashleigh-T <ash0606@me.com>
2024-10-18 14:01:51 -0700

    added basic constructors and new inst var

--- Student.java
+++ Student.java
@@ -1,3 +1,17 @@
-public class Student {
+
+public class Student extends User{
     
+    private int grade;
+    //private Book[] books;
+    
+    public Student(int id, String firstName, String lastName, int acess, int grade){
+        super(id,firstName,lastName,acess);
+        this.grade = grade;
+    }
+
+    public Student(Student other){
+        super(other);
+        this.grade = other.grade;
+    }
+
 }
