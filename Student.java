9f28a655b776d66dd3d925c2870a806c3abd9622
Ashleigh-T <ash0606@me.com>
2024-11-05 12:52:29 -0800

    added a HashMap for the books and a checkOutBook()

--- Student.java
+++ Student.java
@@ -1,17 +1,40 @@
+import java.util.HashMap;
+
 
 public class Student extends User{
     
     private int grade;
-    //private Book[] books;
+        /* using the Integer class not int for values because HashMaps need
+            classes and objects not primatives 
+        */
+    private HashMap<Integer><Book> books = new HashMap<Integer><Book>;
+    private int readingLevel;
     
-    public Student(int id, String firstName, String lastName, int acess, int grade){
+
+    //basic constructor
+    public Student(int id, String firstName, String lastName, int acess,
+                    int grade, int readingLevel){
         super(id,firstName,lastName,acess);
         this.grade = grade;
+        this.readingLevel = readingLevel;
     }
 
+    //basic copy constructor 
     public Student(Student other){
         super(other);
         this.grade = other.grade;
+        this.readingLevel = other.readingLevel;
+    }
+
+    //METHODS   
+    // returns true if the book is added and false if the book is not added 
+    public boolean checkOutBook(Book book){
+        if(books.size() >= 5 && book.readingLevel > this.readingLevel){
+            return false;
+        } else{
+            books.put(Integer(book.ISBN),book);
+            return true;
+        }
     }
 
 }
