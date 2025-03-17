339c446806dfed68229ff8875d965fe422c60d99
Ashleigh-T <ash0606@me.com>
2025-03-17 15:02:13 -0700

    Two tests/files are added and work

---
+++ src/BookTest.java
@@ -0,0 +1,12 @@
+
+
+package src;
+
+import org.junit.*;
+
+public class BookTest {
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+}
---
+++ src/LibComputerTest.java
@@ -0,0 +1,11 @@
+package src;
+
+import org.junit.*;
+
+public class LibComputerTest {
+    @Test 
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+
+}
