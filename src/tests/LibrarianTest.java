7ef6d996b01b9151f595ea5814a13cbf760a1ecb
Ashleigh-T <ash0606@me.com>
2025-08-18 17:34:45 -0700

    deleted files that came down from a bad pull request that turned into a bad merge

--- src/tests/LibrarianTest.java
+++
@@ -1,9 +0,0 @@
-<<<<<<<< HEAD:src/test/LibrarianTest.java
-package src.test;
-========
-package src.tests;
->>>>>>>> 372c6d7 (reorginized file structure):src/tests/LibrarianTest.java
-
-public class LibrarianTest {
-    
-}
--- src/tests/mainTest.java
+++
@@ -1,9 +0,0 @@
-<<<<<<<< HEAD:src/test/mainTest.java
-package src.test;
-========
-package src.tests;
->>>>>>>> 372c6d7 (reorginized file structure):src/tests/mainTest.java
-
-public class mainTest {
-    
-}
